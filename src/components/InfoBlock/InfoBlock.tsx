1e37692f4b1bb34209b04f39fa15344beea60069
Sorokintech <sorokintech@gmail.com>
2023-10-06 00:22:06 +0300

    logo-change

--- src/components/InfoBlock/InfoBlock.tsx
+++ src/components/InfoBlock/InfoBlock.tsx
@@ -6,9 +6,9 @@ const infoMock = [
   {
     header: "Replacement",
     label:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, rerum.",
+      "Checking accounts are given 12 hours. Non-valid returns are made in cases of: ",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam facilis suscipit cupiditate unde cumque odio consequuntur at tempore  exercitationem assumenda?",
+      "▪ checkpointing of the account before the moment of purchase (checkpointing when logging in no replacements)       ▪ polisi or advertising ban before the moment of purchase       ▪ not matching the description      If your case is not suitable write to support      ",
   },
   {
     header: "How to buy?",
@@ -19,24 +19,21 @@ const infoMock = [
   },
   {
     header: "Verify Account",
-    label:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, rerum.",
+    label: "How to avoid advertising restrictions?",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam facilis suscipit cupiditate unde cumque odio consequuntur at tempore  exercitationem assumenda?",
+      "Get an easy way to bypass advertising restrictions using our Telegram bot. Our neural network generates special cards that allow you to successfully pass the process of checking and restoring your advertising activity.",
   },
   {
     header: "Discount",
-    label:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, rerum.",
+    label: "Discounts for bulk purchases are given automatically ",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam facilis suscipit cupiditate unde cumque odio consequuntur at tempore  exercitationem assumenda?",
+      "purchase from 10 accounts - 5%       purchase from 50 accounts - 10%       purchase from 100 accounts - 15%       purchase from 200 accounts - 30%      ",
   },
   {
     header: "Important",
-    label:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, rerum.",
+    label: "Important section",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam facilis suscipit cupiditate unde cumque odio consequuntur at tempore  exercitationem assumenda?",
+      "We have been producing Facebook accounts for marketing, traffic arbitrage and dropshipping since 2018. We have written a bot that farming accounts using artificial intelligence, making each account unique",
   },
 ];
 
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,19 +1,14 @@
 import { FC } from "react";
 import cn from "classnames";
 
-import LogoIcon from "../../assets/logo-scream.svg";
+import LogoImage from "../../assets/images/logo.png";
 
 import "./Logo.scss";
 
 const Logo: FC = () => {
   return (
     <div className="logo-block">
-      <img src={LogoIcon} alt="" className={cn("logo-block__image")} />
-      <div className={cn("logo-block__text")}>
-        <p>Holy </p>
-        <p>Farm </p>
-        <p>Shop</p>
-      </div>
+      <img src={LogoImage} alt="" className={cn("logo-block__image")} />
     </div>
   );
 };
